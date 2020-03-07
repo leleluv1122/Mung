daffd5d6017c6753b193cdc28768edce38a46083
leleluv1122 <jh_o214@naver.com>
2020-03-07 21:24:25 +0900

    chatting

---
+++ Chat/src/main/java/net/lele/config/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package net.lele.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import net.lele.handler.SocketHandler;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+	@Autowired
+	SocketHandler socketHandler;
+
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(socketHandler, "/chating");
+	}
+}
